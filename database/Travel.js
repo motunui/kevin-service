6d95dbd93c3a0018bb261d73b40d529a9d2e5a58
kgxu <Kevinx88@gmail.com>
2019-02-07 20:01:19 -0500

    Update server to shuffle randomId on item page

--- database/Travel.js
+++ database/Travel.js
@@ -3,9 +3,14 @@ var sqlite3 = Promise.promisifyAll(require('sqlite3').verbose());
 var Travel = new sqlite3.Database('./travel.db');
 
 var travelData = function() {
-  return Travel.all(`SELECT * FROM events WHERE id=1`, function(err, rows) {
-    if (err) console.error(err);
-    else return rows;
+  var randomId = Math.round(Math.random() * 100);
+  return Travel.all(`SELECT * FROM events WHERE id=${randomId}`, function(err, rows) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(rows);
+      return rows;
+    }
   });
 }
 
